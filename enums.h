7c962cc362217783b5c495fe3dd86a3d07e3d55b
sin_een <sineen.nawatha@mail.huji.ac.il>
2018-01-14 14:52:38 +0200

    compiling :) and prints ok :D :D :D :D

---
+++ enums.h
@@ -0,0 +1,18 @@
+//
+// Created by sin_een on 1/14/18.
+//
+
+#ifndef EX2_ENUMS_H
+#define EX2_ENUMS_H
+
+enum piece_type
+{
+    queen, king, bishop, knight, rook, pawn
+};
+
+enum piece_color
+{
+    black, white
+};
+
+#endif //EX2_ENUMS_H
